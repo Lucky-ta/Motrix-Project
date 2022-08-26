3c892cb74ee9241f0b16665d18ccbb4e51258540
Lukita <lucmaieski@gmail.com>
2022-08-26 13:21:02 -0300

    Create currentContent context

--- src/contexts/MyContext.ts
+++ src/contexts/MyContext.ts
@@ -1,14 +1,32 @@
 /* eslint-disable no-unused-vars */
 import { createContext } from 'react';
 
-interface MyContextShape {
-    isModalVisible: boolean;
-    setIsModalVisible: (value: boolean) => void;
+export type SubjectShape = {
+  id: number;
+  name: string;
+  description: string;
+  timestamps: string;
 }
+
+interface MyContextShape {
+  isModalVisible: boolean;
+  setIsModalVisible: (value: boolean) => void;
+  currentContent: SubjectShape;
+  setCurrentContent: (value: SubjectShape) => void;
+}
+
+export const subjectIniitalValue: SubjectShape = {
+  id: 0,
+  name: '',
+  description: '',
+  timestamps: '',
+};
 
 const data: MyContextShape = {
   isModalVisible: false,
   setIsModalVisible: () => {},
+  currentContent: subjectIniitalValue,
+  setCurrentContent: () => {},
 };
 
 const MyContext = createContext<MyContextShape>(data);
--- src/contexts/MyProvider.tsx
+++ src/contexts/MyProvider.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/jsx-no-constructed-context-values */
 import React, { useState } from 'react';
-import MyContext from './MyContext';
+import MyContext, { subjectIniitalValue } from './MyContext';
 
 interface MyProviderPropsShape {
     children: React.ReactNode
@@ -8,10 +8,13 @@ interface MyProviderPropsShape {
 
 function MyProvider({ children }:MyProviderPropsShape) {
   const [isModalVisible, setIsModalVisible] = useState(false);
+  const [currentContent, setCurrentContent] = useState(subjectIniitalValue);
 
   const data = {
     isModalVisible,
     setIsModalVisible,
+    currentContent,
+    setCurrentContent,
   };
 
   return (
